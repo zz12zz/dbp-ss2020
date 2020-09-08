08fece3485c7b230166f8bcfff3206c1ad5f5b24
zz12zz <70574149+zz12zz@users.noreply.github.com>
2020-09-08 18:42:27 +0900

    Add files via upload

---
+++ W02-P/process_create.php
@@ -0,0 +1,21 @@
+<<?php
+  $link = mysqli_connect("localhost","root","rootroot","dbp_hw");
+
+  $query = "
+    INSERT INTO topic (
+      title, description,created
+      ) VALUE (
+        '{$_POST['title']}',
+        '{$_POST['description']}',
+        now()
+      )
+  ";
+
+  $result = mysqli_query($link,$query);
+  if($result==false){
+    echo '저장하는 과정에서 문제가 발생했습니다. 관리자에게 문의하세요.';
+    error_log(mysql_error($link));
+  } else {
+    echo '성공했습니다. <a href="index.php">돌아가기</a>';
+  }
+ ?>
