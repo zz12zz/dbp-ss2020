0f4a0315a44ffd27e0cf8d0908c650fffa191ca4
zz12zz <70574149+zz12zz@users.noreply.github.com>
2020-09-15 17:30:59 +0900

    Add files via upload

---
+++ w03-P/index.php
@@ -0,0 +1,48 @@
+<?php
+  $link = mysqli_connect('localhost', 'root', 'rootroot', 'dbp_hw');
+  $query = "SELECT * FROM topic";
+  $result = mysqli_query($link, $query);
+  $list ='';
+  while ($row = mysqli_fetch_array($result)) {
+      $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+  }
+  $article = array(
+    'title' => 'Welcome',
+    'description' => '컴퓨터공학과 수업은...'
+  );
+  $update_link = '';
+  $delete_link = '';
+  if (isset($_GET['id'])) {
+      $query = "SELECT * FROM topic WHERE id={$_GET['id']}";
+      $result = mysqli_query($link, $query);
+      $row = mysqli_fetch_array($result);
+      $article = array(
+      'title' => $row['title'],
+      'description' => $row['description']
+    );
+      $update_link = '<a href="update.php?id='.$_GET['id'].'">update</a>';
+      $delete_link = '
+      <form action="process_delete.php" method="POST">
+        <input type="hidden" name="id" value="'.$_GET['id'].'">
+        <input type ="submit" value="delete">
+        </form>
+    ';
+  }
+ ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>컴퓨터공학과</title>
+  </head>
+  <body>
+    <h1><a href="index.php">컴퓨터공학과 수업</a></h1>
+    <ol><?=$list ?></ol>
+<a href="create.php">create</a>
+<?=$update_link?>
+<?=$delete_link?>
+   <h2><?= $article['title'] ?></h2>
+   <?= $article['description'] ?>
+  </body>
+</html>
---
+++ w03-P/update.php
@@ -0,0 +1,44 @@
+<?php
+  $link = mysqli_connect('localhost', 'root', 'rootroot', 'dbp_hw');
+  $query = "SELECT * FROM topic";
+  $result = mysqli_query($link, $query);
+  $list ='';
+  while ($row = mysqli_fetch_array($result)) {
+      $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+  }
+  $article = array(
+    'title' => 'Welcome',
+    'description' => '컴퓨터공학과 수업은 ...'
+  );
+
+  if (isset($_GET['id'])) {
+      $filtered_id = mysqli_real_escape_string($link, $_GET['id']);
+      $query = "SELECT * FROM topic WHERE id={$filtered_id}";
+      $result = mysqli_query($link, $query);
+      $row = mysqli_fetch_array($result);
+      $article = array(
+      'title' => $row['title'],
+      'description' => $row['description']
+    );
+  }
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>컴퓨터공학과</title>
+   </head>
+   <body>
+     <h1><a href="index.php">컴퓨터공학과 수업</a></h1>
+     <ol><?= $list ?></ol>
+  <form action="process_update.php" method="POST">
+    <input type ="hidden" name="id" value="<?=$filtered_id?>"
+    <p><input type="text" name = "title" placeholder="title"
+      value="<?=$article['title']?>"></p>
+    <p><textarea name="description" placeholder="description">
+    <?=$article['description']?></textarea></p>
+    <p><input type="submit"></p>
+  </form>
+</body>
+</html>
